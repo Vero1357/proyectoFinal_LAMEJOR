92404ef6eea4926c79fc15914f06b923e84725c8
Vero1357 <veroyopo2010@gmail.com>
2024-06-16 19:18:49 -0300

    actualizo de acuerdo a la última clase, sigue dando error

--- prueba/app.py
+++ prueba/app.py
@@ -25,7 +25,7 @@ class Catalogo:
         except mysql.connector.Error as err:
 
             if err.errno == mysql.connector.errorcode.ER_BAD_DB_ERROR:
-                self.cursor.execute(f"CREATE DATABASE IF NOT EXISTS {database}") 
+                self.cursor.execute(f"CREATE DATABASE IF NOT EXISTS {database}")                 
                 self.conn.database = database
             else:
                 raise err  
@@ -40,136 +40,75 @@ class Catalogo:
              
         self.conn.commit()
 
-        self.cursor.close ()
+        self.cursor.close()
         self.cursor = self.conn.cursor(dictionary=True)
 
-    #     # -------------------------------------------------------------------
-    #     #Método para agregar un producto   ----QUE HIZO EL PROFE-----         
-    #     # -------------------------------------------------------------------
-    # def agregar_producto(self, descripcion, cantidad, precio, imagen_url, proveedor):
+        # -------------------------------------------------------------------
+        #Método para agregar un producto       
+        # -------------------------------------------------------------------
+    def agregar_producto(self, descripcion, cantidad, precio, imagen_url, proveedor):
 
-    #     sql =  "INSERT INTO productos (descripcion, cantidad, precio, imagen_url, proveedor) VALUES (%s, %s, %s, %s, %s)"   
-    #     # Los valores se pasan como parámetros separados a la consulta, lo que asegura que sean tratados como datos y no como parte del código SQL. Los marcadores de posición %s son reemplazados por los valores reales de los parámetros cuando se ejecuta la consulta.
+        sql = "INSERT INTO productos (descripcion, cantidad, precio, imagen_url, proveedor) VALUES (%s, %s, %s, %s, %s)"   
+        # Los valores se pasan como parámetros separados a la consulta, lo que asegura que sean tratados como datos y no como parte del código SQL. Los marcadores de posición %s son reemplazados por los valores reales de los parámetros cuando se ejecuta la consulta.
         
-    #     valores = (descripcion, cantidad, precio, imagen_url, proveedor)        
+        valores = (descripcion, cantidad, precio, imagen_url, proveedor)        
     
-    #     self.cursor.execute(sql, valores) #Ejecuto
-    #     self.conn.commit() #Guardo
-    #     return self.cursor.lastrowid #proporciona el valor de la clave primaria generada automáticamente por la base de datos para la fila recién insertada.
+        self.cursor.execute(sql, valores) 
+        self.conn.commit() 
+        return self.cursor.lastrowid #proporciona el valor de la clave primaria generada automáticamente por la base de datos para la fila recién insertada.
 
-    def agregar_producto (self, codigo, descripcion, cantidad, precio, imagen, proveedor):
-    
-        if self.consultar_producto(codigo):
-            return False
-    
-        nuevo_producto = {
-           'codigo' : codigo,
-           'descripcion' : descripcion,
-           'cantidad' : cantidad,
-           'precio' : precio, 
-           'imagen' : imagen,
-           'proveedor' : proveedor
-        }
-
-        self.productos.append(nuevo_producto)
-        return True
-
-
-    # # -------------------------------------------------------------------
-    # # Método para consultar un producto a partir de su código -- QUE HIZO EL PROFE -- 
-    # # -------------------------------------------------------------------
-    # def consultar_producto(self, codigo):
-    #     # Consultamos un producto a partir de su código
-    #     self.cursor.execute(f"SELECT * FROM productos WHERE codigo = {codigo}")
-    #     return self.cursor.fetchone()  #fetchone devuelve un sólo registro
-
-
+   
+    # -------------------------------------------------------------------
+    # Método para consultar un producto a partir de su código 
+    # -------------------------------------------------------------------
     def consultar_producto(self, codigo):
-        for producto in self.productos:
-            if producto['codigo'] == codigo:
-                return producto
-        return False
-
-
-
-    # # -------------------------------------------------------------------
-    # # Método para modificar los datos de un producto a partir de su código -- QUE HIZO EL PROFE -- 
-    # # -------------------------------------------------------------------
-    # def modificar_producto(self, codigo, nueva_descripcion, nueva_cantidad, nuevo_precio, nueva_imagen, nuevo_proveedor):
-
-    #     sql = "UPDATE productos SET descripcion=%s, cantidad=%s, precio=%s, imagen_url=%s, proveedor=%s WHERE codigo=%s"
-
-    #     valores = (nueva_descripcion, nueva_cantidad, nuevo_precio, nueva_imagen, nuevo_proveedor, codigo)
-    #     self.cursor.execute(sql, valores)
-    #     self.conn.commit() #Guardo
-    #     return self.cursor.rowcount > 0
-    #     #rowCount() se utiliza para comprobar si una operación SQL ha afectado a alguna fila en la base de datos. Es una comparación que verifica si este número es mayor que cero, indica que al menos una fila fue afectada.    
+        # Consultamos un producto a partir de su código
+        self.cursor.execute(f"SELECT * FROM productos WHERE codigo = {codigo}")
+        return self.cursor.fetchone()  #fetchone devuelve un sólo registro
 
 
 
 
-    def modificar_producto (self, codigo, nueva_descripcion, nueva_cantidad, nuevo_precio, nueva_imagen, nuevo_proveedor):
-        for producto in self.productos:
-            if producto ['codigo'] == codigo:
-                producto ['descripcion'] = nueva_descripcion
-                producto ['cantidad'] = nueva_cantidad
-                producto ['precio'] = nuevo_precio
-                producto ['imagen'] = nueva_imagen
-                producto ['proveedor'] = nuevo_proveedor
-            return True
-        return False
+    # -------------------------------------------------------------------
+    # Método para modificar los datos de un producto a partir de su código 
+    # -------------------------------------------------------------------
+    def modificar_producto(self, codigo, nueva_descripcion, nueva_cantidad, nuevo_precio, nueva_imagen, nuevo_proveedor):
+
+        sql = "UPDATE productos SET descripcion=%s, cantidad=%s, precio=%s, imagen_url=%s, proveedor=%s WHERE codigo=%s"
+
+        valores = (nueva_descripcion, nueva_cantidad, nuevo_precio, nueva_imagen, nuevo_proveedor, codigo)
+        self.cursor.execute(sql, valores)
+        self.conn.commit() 
+        return self.cursor.rowcount > 0
+        #rowCount() se utiliza para comprobar si una operación SQL ha afectado a alguna fila en la base de datos. Es una comparación que verifica si este número es mayor que cero, indica que al menos una fila fue afectada.    
 
     
     
-    # # -------------------------------------------------------------------
-    # # Método para obtener un listado de los productos en pantalla -- QUE HIZO EL PROFE --- 
-    # # -------------------------------------------------------------------
-    # def listar_productos(self):
-    #     self.cursor.execute("SELECT * FROM productos")
-    #     productos = self.cursor.fetchall() #devuelve todas las filas en una consulta SQL
-    #     return productos
-
-
-
+    # -------------------------------------------------------------------
+    # Método para obtener un listado de los productos en pantalla 
+    # -------------------------------------------------------------------
     def listar_productos(self):
-        print("-"*50)
-        for producto in self.productos:
-          print(f"Codigo     :{producto['codigo']}")
-          print(f"Descripcion:{producto['descripcion']}")
-          print(f"Cantidad   :{producto['cantidad']}")
-          print(f"Precio     :{producto['precio']}")
-          print(f"Imagen     :{producto['imagen']}")
-          print(f"Proveedor  :{producto['proveedor']}")
-          print("-"*50)   
+        self.cursor.execute("SELECT * FROM productos")
+        productos = self.cursor.fetchall() #devuelve todas las filas en una consulta SQL
+        return productos
 
 
-    # # -------------------------------------------------------------------
-    # # Método para eliminar un producto a partir de su código -- QUE HIZO EL PROFE -- 
-    # # -------------------------------------------------------------------
-    # def eliminar_producto(self, codigo):
-    #     # Eliminamos un producto de la tabla a partir de su código
-    #     producto_eliminado = producto['descripcion']
-    #     print(f"Producto {producto_eliminado} eliminado.")
 
-    #     self.cursor.execute(f"DELETE FROM productos WHERE codigo = {codigo}")
-    #     self.conn.commit() #Guardo
-    #     return self.cursor.rowcount > 0
-    #     #rowCount() se utiliza para comprobar si una operación SQL ha afectado a alguna fila en la base de datos. Es una comparación que verifica si este número es mayor que cero, indica que al menos una fila fue afectada.  
+    # -------------------------------------------------------------------
+    # Método para eliminar un producto a partir de su código 
+    # -------------------------------------------------------------------
+    def eliminar_producto(self, codigo):
+        # Eliminamos un producto de la tabla a partir de su código
+        producto_eliminado = producto['descripcion']
+        print(f"Producto {producto_eliminado} eliminado.")
+
+        self.cursor.execute(f"DELETE FROM productos WHERE codigo = {codigo}")
+        self.conn.commit() 
+        return self.cursor.rowcount > 0
+        #rowCount() se utiliza para comprobar si una operación SQL ha afectado a alguna fila en la base de datos. Es una comparación que verifica si este número es mayor que cero, indica que al menos una fila fue afectada.  
                 
 
 
-
-    def eliminar_producto (self, codigo):
-        for producto in self.productos:
-            if producto ['codigo'] == codigo:
-                self.productos.remove(producto)
-                print()
-                print("Producto eliminado correctamente")
-                return True
-                print()
-                print ("Producto no encontrado")
-        return False
-    
     def mostrar_producto (self, codigo):
         producto = self.consultar_producto(codigo)
         if producto:
@@ -185,20 +124,20 @@ class Catalogo:
             print("Producto no encontrado.")
 
 #Programa principal
-
 catalogo = Catalogo(host='localhost', user='root', password='root', database='lamejor')
+
 
 #Agregamos productos
 
-catalogo.agregar_producto(15, "Sorrentinos", 10, 1500, "sorrentinos.jpeg", 1004)
-catalogo.agregar_producto(20, "Asado", 25, 2500, "asado_tira.jpeg", 2003)
-catalogo.agregar_producto(11,"Bife", 15, 6700, "bife.jpg", 2003)
-catalogo.agregar_producto(12, "Chorizo", 30, 1200, "chorizo.jpeg", 2003)
-catalogo.agregar_producto(13, "Empanadas", 43, 700, "empanadas.jpeg", 3001)
-catalogo.agregar_producto(14, "locro", 8, 9800, "locro.jpeg", 1004)
+catalogo.agregar_producto("Sorrentinos", 10, 1500, "sorrentinos.jpeg", 1004)
+catalogo.agregar_producto("Asado", 25, 2500, "asado_tira.jpeg", 2003)
+catalogo.agregar_producto("Bife", 15, 6700, "bife.jpg", 2003)
+catalogo.agregar_producto("Chorizo", 30, 1200, "chorizo.jpeg", 2003)
+catalogo.agregar_producto("Empanadas", 43, 700, "empanadas.jpeg", 3001)
+catalogo.agregar_producto("locro", 8, 9800, "locro.jpeg", 1004)
 print()
 # print("listado de productos:")
-# catalogo.listar_productos()
+# productos = catalogo.listar_productos()
 # print()
 # print("datos de un producto:")
 # catalogo.mostrar_producto(20)
@@ -207,7 +146,6 @@ print()
 # print("listado de productos:")
 
 
-# A CONTINUACIÓN ES LO QUE HIZO EL PROFESOR !!!
 
 #--------------------------------------------------------------------
 # Listar todos los productos
@@ -240,13 +178,6 @@ def mostrar_producto(codigo):
 # productos = catalogo.listar_productos()
 # for producto in productos:
 #     print(producto)
-
-
-
-
-
-
-
 
 
 # ----------------------- FLASK -------------------------
